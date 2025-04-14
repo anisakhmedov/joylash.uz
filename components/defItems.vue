6de401aeb665188f5bebd4de1b5da99595738e24
Anis Akhmedov <akhmedovanis5@gmail.com>
2025-04-14 21:07:31 +0500

    asd

--- components/defItems.vue
+++ components/defItems.vue
@@ -8,25 +8,24 @@
             <div class="info">
                 <div class="title">
                     <div class="left">
-                        <p>ParadisePoint Villa</p>
                         <div class="price">
                             <span class="mainPrice">$1.980</span>
-                            <span class="duration">/month</span>
-                            <div class="disc">
-                                Lorem ipsum dolor sit amet consectetur. Dis augue faucibus egestas tristique ullamcorper.
-                            </div>
+                        </div>
+                        <p>ParadisePoint Villa</p>
+                        <div class="disc">
+                            Lorem ipsum dolor sit amet consectetur. Dis augue faucibus egestas tristique ullamcorper.
                         </div>
                     </div>
                     <div class="right">
                         <img src="~/public/icons/heart.svg" alt="">
                     </div>
                 </div>
+                <hr>
                 <div class="options">
                     <ul>
-                        <li><img src="~/public/icons/beds.svg" alt=""><span>2 Beds</span></li>
-                        <li><img src="~/public/icons/shower.svg" alt=""><span>2 Baths</span></li>
-                        <li><img src="~/public/icons/size.svg" alt=""><span>200m<sup>2</sup></span></li>
-                        <li><img src="~/public/icons/garage.svg" alt=""><span>2 Parking Lot</span></li>
+                        <li><img src="~/public/icons/bed-org.svg" alt=""><span>2 Beds</span></li>
+                        <li><img src="~/public/icons/shower-org.svg" alt=""><span>2 Baths</span></li>
+                        <li><img src="~/public/icons/scale-org.svg" alt=""><span>200m<sup>2</sup></span></li>
                     </ul>
                 </div>
             </div>
